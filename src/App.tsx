3f1d4918287da0e0d339c2311fa16dba779dad8c
Leticia Godoy <let.godoy@live.com>
2022-01-18 21:14:59 -0300

    hello word

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with styled-components and TypeScript
+          HELLO WORLD
         </Typography>
         <ProTip />
         <Copyright />
