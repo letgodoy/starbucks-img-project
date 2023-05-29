65e1aa443671d509efad0d34a7ac28d4ab0b17e1
John Vilela <johnvvilela@gmail.com>
2023-05-29 10:36:42 -0300

    fix: bloquear acao de usuario na listagem por regra

--- src/pages/list-user/index.tsx
+++ src/pages/list-user/index.tsx
@@ -4,6 +4,7 @@ import PlayIcon from '@mui/icons-material/PlayCircleOutline';
 import { Box, Button, IconButton, Skeleton, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
 import { Layout, checkBrand } from "../../components";
+import { useAuth } from '../../components/contexts/auth';
 import { useGetAllUsers, useToggleBlockUser } from "../../dataAccess";
 import { UserRoles } from '../../enums/UserRoles';
 import { useValidateUserRole } from '../../hooks/useValidateUserRole';
@@ -13,13 +14,29 @@ export const ListUser = ({ params }: any) => {
   const canCreate = useValidateUserRole([UserRoles.ADMIN, UserRoles.DISTRICTMANAGER, UserRoles.MANAGERAGENCY, UserRoles.MANAGERPHOTO, UserRoles.MANAGERSTORE, UserRoles.OPERATIONMANAGER]);
 
   checkBrand()
-  
+  const { user } = useAuth();
   const navigate = useNavigate();
   const { data, status: getUserStatus } = useGetAllUsers();
   const { mutateAsync, status: toggleBlockStatus } = useToggleBlockUser();
 
   const isLoading = getUserStatus === "loading" || toggleBlockStatus === "loading";
   const canRenderTable = getUserStatus === "success" && data && data?.length > 0;
+
+  const userCanDoAction = (role: UserRoles) => {
+    if (user.role === UserRoles.ADMIN) return true;
+
+    if (user.role === UserRoles.OPERATIONMANAGER && role !== UserRoles.ADMIN) return true;
+
+    if (user.role === UserRoles.DISTRICTMANAGER && role !== UserRoles.ADMIN && role !== UserRoles.OPERATIONMANAGER) return true;
+
+    if (user.role === UserRoles.MANAGERSTORE && role === UserRoles.MANAGERSTORE) return true;
+
+    if (user.role === UserRoles.MANAGERAGENCY && (role === UserRoles.MANAGERAGENCY || role === UserRoles.USERAGENCY)) return true;
+
+    if (user.role === UserRoles.MANAGERPHOTO && (role === UserRoles.MANAGERPHOTO || role === UserRoles.USERPHOTO)) return true;
+
+    return false;
+  }
 
   if (isLoading) return (
     <Layout params={params}>
@@ -51,17 +68,23 @@ export const ListUser = ({ params }: any) => {
                 <TableCell>{u.name}</TableCell>
                 <TableCell>{u.email}</TableCell>
                 <TableCell>
-                  <IconButton edge="end" aria-label="edit" onClick={() => navigate(`/editar-usuario/${u.uid}`)}>
-                    <EditIcon />
-                  </IconButton>
-                  {u.isBlocked ? (
-                    <IconButton edge="end" aria-label="delete" onClick={() => mutateAsync(u.uid)}>
-                      <PlayIcon />
-                    </IconButton>
+                  {userCanDoAction(u.role as UserRoles) ? (
+                    <>
+                      <IconButton edge="end" aria-label="edit" onClick={() => navigate(`/editar-usuario/${u.uid}`)}>
+                        <EditIcon />
+                      </IconButton>
+                      {u.isBlocked ? (
+                        <IconButton edge="end" aria-label="delete" onClick={() => mutateAsync(u.uid)}>
+                          <PlayIcon />
+                        </IconButton>
+                      ) : (
+                        <IconButton edge="end" aria-label="delete" onClick={() => mutateAsync(u.uid)}>
+                          <BlockIcon />
+                        </IconButton>
+                      )}
+                    </>
                   ) : (
-                    <IconButton edge="end" aria-label="delete" onClick={() => mutateAsync(u.uid)}>
-                      <BlockIcon />
-                    </IconButton>
+                    u.isBlocked ? <BlockIcon /> : <></>
                   )}
                 </TableCell>
               </TableRow>
